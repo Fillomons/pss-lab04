a4bd6a46ea3b4b9c3300753379eb7cca2d963995
Filippo Monti <filippomonti2003@gmail.com>
2023-10-17 12:06:00 +0200

    Setter color lamp create and Info lamp

--- 01-encapsulation/src/main/java/it/unibo/encapsulation/SmartLamp.java
+++ 01-encapsulation/src/main/java/it/unibo/encapsulation/SmartLamp.java
@@ -12,7 +12,7 @@ public class SmartLamp{
         this.color='White';//white, red, blue e green
     }
 
-    public SmartLamp(boolean SstatoOnOff, int Iintensity, int Ccolor){
+    public SmartLamp(boolean SstatoOnOff, int Iintensity, String Ccolor){
         this.statoOnOff=SstatoOnOff;
         if(Iintensity<1 && Iintensity>3){
             System.out.println("Valore errato");
@@ -22,33 +22,33 @@ public class SmartLamp{
             this.intensity=Iintensity;
         }
 
-        switch(Ccolor){
-            case "White":
-                this.color=Ccolor;
-                break;
-            case "Green":
-                this.color=Ccolor;
-                break;
-            case "Red":
-                this.color=Ccolor;
-                break;
-            case "blue":
-                this.color=Ccolor;
-                break;
-        }
+        color(Ccolor);
 
     }
 
+    public boolean getstatoOnOff(){
+        return this.statoOnOff;
+    }
+
+    public boolean getintensity(){
+        return this.intensity;
+    }
+
+    public boolean getcolor(){
+        return this.color;
+    }  
+
     //aggiunta setter
     public void ONstatoOnOff(){
-        if(this.statoOnOff==false){
+        /*if(this.statoOnOff==false){
             this.statoOnOff= true;
             System.out.println("Accesa");
         }
         else{//offstatoONOff
             this.statoOnOff= false;
             System.out.println("Spenta");
-        }
+        }*/
+        this.statoOnOff=!this.statoOnOff;
     }
 
     public void calculationintensity(){
@@ -63,5 +63,27 @@ public class SmartLamp{
         }
     }  
 
+    public void color(String Ccolor){
+        switch(Ccolor){
+            case "White":
+                this.color=Ccolor;
+                break;
+            case "Green":
+                this.color=Ccolor;
+                break;
+            case "Red":
+                this.color=Ccolor;
+                break;
+            case "blue":
+                this.color=Ccolor;
+                break;
+        }
+    }
+
+    public void printAll(){
+        System.out.println("\n Status: "+getstatoOnOff());
+        System.out.println("\n Color: "+getcolor());
+        System.out.println("\n Intnsity: "+getintensity());
+    }
 
 }
