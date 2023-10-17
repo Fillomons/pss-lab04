c79865c181088c2a451b6fe310797d0e705a0b3f
Filippo Monti <filippomonti2003@gmail.com>
2023-10-17 11:46:11 +0200

    Setter intensity lamp create

--- 01-encapsulation/src/main/java/it/unibo/encapsulation/SmartLamp.java
+++ 01-encapsulation/src/main/java/it/unibo/encapsulation/SmartLamp.java
@@ -41,15 +41,27 @@ public class SmartLamp{
 
     //aggiunta setter
     public void ONstatoOnOff(){
-        if(this.statoOnOff=false){
+        if(this.statoOnOff==false){
             this.statoOnOff= true;
             System.out.println("Accesa");
         }
-        else{
+        else{//offstatoONOff
             this.statoOnOff= false;
             System.out.println("Spenta");
         }
-    }   
+    }
+
+    public void calculationintensity(){
+        if(this.intensity==1){
+            System.out.println("Intensità bassa");
+        }
+        else if(this.intensity==2){
+            System.out.println("Intensità media");
+        }
+        else{
+            System.out.println("Intensità alta");
+        }
+    }  
 
 
 }
