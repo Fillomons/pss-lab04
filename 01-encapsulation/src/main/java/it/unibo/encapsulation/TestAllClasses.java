0d66df1c1f4ac2c6720e6d8f72c393e1367165ff
Filippo Monti <filippomonti2003@gmail.com>
2023-10-31 10:37:29 +0100

    fase 3 completed

--- 01-encapsulation/src/main/java/it/unibo/encapsulation/TestAllClasses.java
+++ 01-encapsulation/src/main/java/it/unibo/encapsulation/TestAllClasses.java
@@ -16,5 +16,31 @@ public class TestAllClasses {
         final Smartphone note3 = new Smartphone(4, 2048, 8192, "Samsung", "Galaxy Note 3", true, true, true);
         htcOne.printStringRep();
         note3.printStringRep();
+
+
+        SmartLamp lamp = new SmartLamp();
+        //non passo nessun valore
+        System.out.println(lamp.getstatoOnOff());
+        System.out.println(lamp.getintensity());
+        System.out.println(lamp.getcolor());
+
+
+        if (lamp.isOn()==true)
+            System.out.println("accesa");
+        
+        System.out.println(lamp.getstatoOnOff());
+        lamp.setIntensity(1);
+        System.out.println(lamp.getintensity());
+        lamp.setIntensity(2);
+        System.out.println(lamp.getintensity());
+        lamp.setIntensity(3);
+        System.out.println(lamp.getintensity());
+
+        System.out.println(lamp.getcolor());
+        lamp.setColor("green");
+        System.out.println(lamp.getcolor());
+        if(lamp.isOn()==false)
+        System.out.println("spenata");
+        
     }
 }
